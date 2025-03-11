247202ff8db264eebdcba1518114ef844979e596
bjrea <bjrea@uiowa.edu>
2025-03-11 13:33:01 -0500

    update

---
+++ Midterm_Review/MidtermReview.hs
@@ -0,0 +1,39 @@
+{-
+
+Exam Format:
+
+pen-and-paper programming questions
+knowledge questions
+prove properties about programs by using equivalences between Haskell expressions and by using induction
+
+Important Exam Topics:
+
+Evaluation of Haskell expressions, laziness
+    Understand well how Haskell expressions are evaluated, and the idea and impact of lazy evaluation.
+
+Guards vs. pattern matching
+    Be familiar with the differences between guards and pattern matching. 
+    Understand, how guards can be used to deconstruct data and to select a function case.
+
+Recursion to solve problems
+    Be able to implement recursive algorithms.  
+    Perform recursion over recursive datatypes, such as Lists, or other user-defined datatypes.
+
+Data types and type synonyms
+    Be able to define your own datatypes and work with them. 
+    Be familiar with the differences between types (data) and type synonyms (type).
+
+Standard functions (map, filter, etc.)
+    Be able to use the common standard functions we saw in the lecture.
+
+Typeclasses, instances, and common typeclasses
+    Typeclasses are a key concept in Haskell.
+    Be familiar with how they function, how they are defined, and how you can implement one for a custom datatype.
+    We also saw some common typeclasses. Understanding those are important.
+
+Reasoning about functions
+    Haskell differs in a key aspect from other programming languages: functions behave like mathematical functions. 
+    They don’t have any side effects. This allows us to reason about Haskell functions, like reasoning about mathematical functions. 
+    You should be able to do some simple proofs and use induction.
+
+-}
