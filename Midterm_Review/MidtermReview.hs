d94ca6a529977a9f059bc2ab057108b1451644bb
bjrea <bjrea@uiowa.edu>
2025-03-11 13:38:57 -0500

    update

--- Midterm_Review/MidtermReview.hs
+++ Midterm_Review/MidtermReview.hs
@@ -37,3 +37,128 @@ Reasoning about functions
     You should be able to do some simple proofs and use induction.
 
 -}
+
+{-
+
+Regular Expressions
+
+import Prelude hiding (((<>), (<|>), words)
+
+import Data.List ((\\))
+
+-- Regex means regular expression
+type Regex = [String]
+
+alphabet :: [Char]
+alphabet = ['a'..'z'] ++ ['A'..'Z'] ++ [' ']
+
+char, noChars :: [Char] -> Regex
+
+
+chars cs = [  [c]  | c <- cs]
+notChars cs = [  [c]  | c <- alphabet \\ cs]
+
+anyChars :: Regex
+anyChars = chars alphabet
+
+none :: Regex
+none = []
+
+empty :: Regex
+empty = [""]
+
+(<|>) :: Regex -> Regex -> Regex
+r <|> s = r ++ s
+
+cross :: (a -> b -> c) -> [a] -> [b] -> [c]
+cross f xs ys = concatMap withY ys where
+    withX y x = f x y
+    withY y = map (withX y) xs
+
+-- r <|> none = r
+plus, question, star :: Regex -> Regex
+
+plus r = r <> (star r)
+question r = r <|> empty
+star r = question (plus r)
+
+main = return ()
+
+-}
+
+{-
+
+Type Classes
+
+:type (== 5)
+:type (== (5 :: Int))
+
+elem :: Eq a => a -> [a] -> Bool
+elem x [] = False
+elem x (y:ys)
+    | x == y    = True
+    | otherwise = elem x ys
+
+-- partially applying
+
+:type (elem (5 :: Int))
+-- (elem (5 :: Int)) :: [Int] -> Bool
+
+eitherElem :: Eq a => a -> [a] -> [a] -> Bool
+eitherElem x xs ys 
+    | elem x xs = True
+    | elem x ys = True
+    | otherwise = False
+
+
+class Eq a => threeEq a where
+    teq :: a -> a -> a -> Bool
+    teq x y z
+        | x == y && y == z = True
+        | otherwise = False
+    notteq :: a -> a -> a -> Bool
+    notteq x y z = not (teq x y z)
+
+instance threeEq Int where
+instance threeEq Char where
+instance Eq a => threeEq [a] where
+instance threeEq Bool where
+
+    -- teq x y z
+    -- below can be a default implementation in the class def
+        -- | x == y && y == z = True
+        -- | otherwise = False
+    --below can be a default implementation in the class def
+    -- notteq x y z = not (teq x y z)
+
+teq (5 :: Int) 5 5
+--True
+teq (5 :: Int) 5 6
+--False
+
+--hoogle.haskell.org
+
+map (+ 2) [1, 2, 3, 4]
+-- [3, 4, 5, 6]
+
+:intances []
+
+fmap (+ 2) [1, 2, 3, 4]
+-- [3, 4, 5, 6]
+
+fmap (+ 2) (Just 3)
+-- Just 5
+
+fmap (+ 2) Nothing
+-- Nothing
+
+fmap (+ 2) (* 5)
+-- well defined, but no instance for 'Show (Integer -> Integer)'
+
+(fmap (+ 2) (* 5)) 3
+-- 17
+
+
+
+
+-}
